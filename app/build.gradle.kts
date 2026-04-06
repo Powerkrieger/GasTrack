e511d242fbd68fb0b079f05d9f426f5bede166b9
joost <joost.rehder@uni-rostock.de>
2026-04-06 13:25:54 +0200

    Bump version to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.gastrack"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
